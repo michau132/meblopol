e0e31a30a274615a1ebc685487e4f549f6eefa54
michau <michal1324@hotmail.com>
2019-11-05 15:51:56 +0100

    added html

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,1 +1,22 @@
-console.log('plugins');
+// console.log('plugins');
+// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//   maxZoom: 18,
+//   id: 'mapbox.streets',
+//   accessToken: 'pk.eyJ1IjoiYWRhbTEyNDIiLCJhIjoiY2syZ21raXo1MHI5ajNjcDVzc3ZkemUzNyJ9.76TF4_MU60ksxwmzPs5GUw'
+// }).addTo(mymap);
+
+
+IF(OR(GFP_Stato_del_GID_Anagrafica__c = 'CERTIFICATO',
+  GFP_Stato_del_GID_Anagrafica__c = 'NON CERTIFICATO',
+  GFP_Stato_del_GID_Anagrafica__c = 'CON PRESCRIZIONI',
+  GFP_Stato_del_GID_Anagrafica__c = 'DA ADEGUARE',
+  GFP_Stato_del_GID_Anagrafica__c = 'DA AUTORIZZARE',
+  GFP_Stato_del_GID_Anagrafica__c = 'POSA IN CORSO',
+  GFP_Stato_del_GID_Anagrafica__c = 'DA RISANARE',
+  GFP_Stato_del_GID_Anagrafica__c = 'RISANATO',
+  GFP_Stato_del_GID_Anagrafica__c = 'NLAV CONCLUSO'),
+  "2",
+  "-"
+)
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,29 @@
-console.log('script');
+//  Liv. Irreg. D. N. cavi vs ANA
+
+IF(OR(GFP_Tipologia_Tratta__c == 'Aerea', GFP_Tipologia_Tratta__c == 'aerea'),
+  IF(GFP_SAL_As_Built__r.RecordType.Name == 'SAL',
+    IF(GFP_Numero_cavi_ottici__c - GFP_Numero_cavi_ottici_Anagrafica_GID__c <= 0,
+      IF(GFP_Numero_cavi_ottici__c - GFP_Numero_cavi_ottici_Anagrafica_GID__c == 0,
+        "0",
+        "1"
+      ),
+      IF(AND(ISPICKVAL(GFP_Tipo_Cavi__c, 'P'), OR(ISPICKVAL(GFP_Tipo_Linea__c, 'MT'), ISPICKVAL(GFP_Tipo_Linea__c, 'BT'))),
+        IF(ISPICKVAL(GFP_Tipo_Linea__c, 'MT'),
+          IF(GFP_Numero_cavi_ottici__c - GFP_Numero_cavi_ottici_Anagrafica_GID__c <= 4,
+            "2",
+            "3"
+          ),
+          //dodac kolejnego ifa ale jest popierdolone jakis kevlar nie czaje o co chodzi, zapytac lucka
+        ),
+        IF(AND(ISPICKVAL(GFP_Tipo_Cavi__c, 'F'),GFP_Numero_cavi_ottici__c - GFP_Numero_cavi_ottici_Anagrafica_GID__c > 5),
+          "3",
+          "2"
+        )
+      )
+    ),
+    ''
+  ),
+  ''
+)
+
+
